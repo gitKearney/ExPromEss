68acf2920b053133996021d745ae5239d54f4e07
Kearney Taaffe <Kearney.Taaffe@gmail.com>
2018-01-19 14:31:35 -0600

    Chaning defaults to inform user they need to change the username, password, and database

--- configs/mysql_example.js
+++ configs/mysql_example.js
@@ -4,8 +4,9 @@
  */
 module.exports = {
   // TODO: put contents of this file in a new file called 'mysql.js'. **DO NOT COMMIT mysql.js**
-  host     : 'localhost',
-  user     : 'root',
-  password : 'HappyHappyJoyJoy',
-  database : 'demo'
+  host     : '127.0.0.1',
+  port     : 3306,
+  user     : 'change_user',
+  password : 'change_pass',
+  database : 'change_database'
 }
